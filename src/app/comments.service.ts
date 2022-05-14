b61d9334fbe58f9d52855564520802fbbf387df8
alladoJed <alladoJed@gmail.com>
2022-05-14 19:12:18 +0300

    worked on CRUD reply

--- src/app/comments.service.ts
+++ src/app/comments.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as commentsData from '../assets/json/data.json';
 import { Comment } from './models/comment';
+import { Reply } from './models/reply';
 import { User } from './models/user';
 
 const commentsStorageName = "comments";
@@ -30,30 +31,53 @@ export class CommentsService {
     return [...this.comments];
   }
   // add a new comment
-  addComment(newComment: Comment) : Comment[] {
-    this.comments.push(newComment);
+  addComment(newComment, id?: number) {
+    //console.log(id)
+    if (id) {
+      let comment = this.findComment(id);
+      console.log(comment)
+      let index = this.findCommentIndex(comment);
+      this.comments[index].replies.push(newComment);
+    } else {
+      this.comments.push(newComment);
+    }
+
     return this.updateStorage();
   }
+
   // update  comment
   updateComment(updatedComment: Comment) {
     let index = this.findCommentIndex(updatedComment)
     this.comments[index] = updatedComment;
     return this.updateStorage();
   }
+
   // delete comment
-  deleteComment(id: number) {
-    this.comments = this.comments.filter( comment => { return comment.id != id });
+  deleteComment(newComment) {
+    if (newComment.replyingTo) {
+      let repliedToThisComment = this.findComment(newComment.comment_id);
+      let index = this.findCommentIndex(repliedToThisComment);
+      let insertComment = repliedToThisComment.replies.filter(reply => { return reply.id !== newComment.id })
+      repliedToThisComment.replies = insertComment;
+      this.comments[index] = repliedToThisComment
+    } else {
+      this.comments = this.comments.filter(comment => { return comment.id !== newComment.id });
+      console.log(this.comments)
+    }
     return this.updateStorage()
   }
 
   // update storage after operation
   private updateStorage() {
     localStorage.setItem(commentsStorageName, JSON.stringify(this.comments))
-    return  this.getComments();
+    return this.getComments();
   }
 
   private findCommentIndex(comment) {
     return this.comments.indexOf(comment);
   }
-  
+  private findComment(id) {
+    return this.comments.filter(comment => { return comment.id === id })[0];
+  }
+
 }
--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { CommentsService } from 'src/app/comments.service';
 import { Comment } from 'src/app/models/comment';
+import { Reply } from 'src/app/models/reply';
 import { User } from '../../models/user';
 
 @Component({
@@ -7,32 +9,53 @@ import { User } from '../../models/user';
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, AfterViewInit {
   @Input() currentUser: User;
-  @Output() add = new EventEmitter< Comment >();
+  @Input() replyingTo;
+  @Output() add = new EventEmitter<Comment | Reply>();
+  @Output() onReplyMode = new EventEmitter<Reply>();
+
+  @ViewChild("textarea") textarea: ElementRef
+
   comment: string;
 
-  constructor() { }
+  constructor(private commentService: CommentsService) { }
 
   ngOnInit(): void {
+    if (this.replyingTo) {
+      this.comment = `@${this.replyingTo.user.username} `
+    }
+  }
+
+  ngAfterViewInit() {
+    if (this.replyingTo) {
+      this.textarea.nativeElement.focus();
+    }
   }
 
   addComment() {
-    if(this.comment === "" || this.comment == undefined) {
+    if (this.comment === "" || this.comment == undefined) {
       return;
     }
-    let newComment: Comment = {
-      id: 0,
-      content: this.comment.trim(),
+    const prevalue = {
+      id: Date.now() as number,
+      content: this.comment.trim().replace(`@${this.replyingTo.user.username} `, ''),
       createdAt: new Date().toString(),
+      user: this.currentUser,
       vote: {
         score: 0,
+        voters: []
       },
-      replies: [],
-      user: this.currentUser
     }
-    //console.log(newComment);
-    this.add.emit(newComment);
+    if (this.replyingTo) {
+      let comment_id = this.replyingTo.comment_id ? this.replyingTo.comment_id : this.replyingTo.id;
+      let reply: Reply = { ...prevalue, replyingTo: this.replyingTo.user.username, comment_id: comment_id }
+      this.commentService.addComment(reply, comment_id);
+      this.onReplyMode.emit(reply);
+    } else {
+      let newComment: Comment = { ...prevalue, replies: [] }
+      this.add.emit(newComment)
+    }
     this.comment = "";
   }
 
